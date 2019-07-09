d75e8fc8f97cd5ddffd182aebb7fc348530bfabc
TheComet <alex.murray@gmx.ch>
2019-07-09 21:58:45 +0200

    Adding project files for firmware

---
+++ Firmware/include/n64c/osc.h
@@ -0,0 +1,16 @@
+/*!
+ * @file osc.h
+ * @author TheComet
+ *
+ * Created on July 7, 2019, 10:04 PM
+ */
+
+#ifndef OSC_H
+#define	OSC_H
+
+/*!
+ * @brief Configures all clocks in the oscillator module
+ */
+void init_osc(void);
+
+#endif	/* OSC_H */
---
+++ Firmware/src/main.c
@@ -0,0 +1,13 @@
+/*!
+ * @file main.c
+ * @author TheComet
+ *
+ * Created on July 7, 2019, 9:50 PM
+ */
+#include "n64c/osc.h"
+
+int main(void) {
+    init_osc();
+
+    while (1) {}
+}
